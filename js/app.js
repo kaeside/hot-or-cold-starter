af307f45923f2f221bf50ec1004dda37c0e1ddfa
Kaeside Iwuagwu <kaeside.iwuagwu@gmail.com>
2016-04-25 14:08:11 -0400

    Fix syntax errors

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-	
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -11,22 +11,22 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
-    
+
     //GRABBING DATA ENTERED
     var guessedAnswer =  calculateAnswer ();
     function calculateAnswer() {
       return Math.floor(integerRange(0,100));
-      console.log(guessedAnswer);
+      //console.log(guessedAnswer);
     }
-     
+
      // GENERATION OF RANDOM NUMBERS
      //Math.Random() * value rounds numbers to 1 less than the value, adding 1 rounds it to the value
      var randomNumber = (Math.random()*100)+1;
-     
+
      //Math.Floor rounds randomnumber down to the nearest integer
      var randomInteger = Math.floor(randomNumber);
-     
-     //SUBMISSION OF GUESSES 
+
+     //SUBMISSION OF GUESSES
     var countGuesses = 0;
   $('.hotOrCold').submit(function(event) {
     event.preventDefault();
@@ -35,13 +35,13 @@ $(document).ready(function(){
     $('.guessBox').append("<li>" + something + "</li>");
     $("#count").text(countGuesses);
     $("#userGuess").val('');
-    
-    //PROMPTS FOR GUESSES 
-    
-    //Math.abs returns the absolute value of a number   
+
+    //PROMPTS FOR GUESSES
+
+    //Math.abs returns the absolute value of a number
     var guessedRange = Math.abs(something - guessedAnswer);
     //console.log(distance);
-    
+
     //These statements run pending on how close or far away the guessedAnswer is from the value
 
     if (guessedRange >= 50) {
@@ -67,10 +67,10 @@ $(document).ready(function(){
     }
 
   });
-  
+
   function integerRange(min, max) {
     //Use Math.floor to round to nearest integer
-    return min + Math.floor((Math.random() * (max - min)))
+    return min + Math.floor((Math.random() * (max - min)));
   }
 
   // GENERATION OF NEW PAGE
