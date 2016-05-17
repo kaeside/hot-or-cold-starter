88f5ad75ad657790e068291e1c08b860481a0199
Elias Mason <eliasjmason@gmail.com>
2016-05-17 14:23:48 -0700

    Sets initial state within store creation.

--- js/store.js
+++ js/store.js
@@ -3,5 +3,12 @@ var createStore = redux.createStore;
 
 var reducers = require('./reducers');
 
-var store = createStore(reducers.guessReducer);
+
+var initialTestSate = [{
+    randomNumber: Math.floor((Math.random() * 100) + 1),
+    guesses: [8,6,7,5,3,1,9]
+}];
+
+
+var store = createStore(reducers.guessReducer, initialTestSate);
 module.exports = store;
