9a815b382bb3b2669d94955a0e1b2e756a918c48
Kaeside Iwuagwu <kaeside.iwuagwu@gmail.com>
2016-05-17 10:02:36 -0400

    removed extra comments

--- js/reducers.js
+++ js/reducers.js
@@ -1,16 +1,7 @@
 var actions = require('./actions');
 var update = require('react-addons-update');
 
-//var store = require('./store');
-// store.dispatch(actions.makeGuess('24'));
-// store.getState();
-
 var initialGameState = [];
-
-// var newGame = function(state, action) {
-//
-// //    return state;
-// };
 
 var guessReducer = function(state, action) {
     state = state || initialGameState;
@@ -26,45 +17,14 @@ var guessReducer = function(state, action) {
                 [state.length - 1]: {guesses: {$push: [action.guess]}}
             });
             return newState;
-            // return state.concat({
-            //     guessedAnswer: action.guess
-            // });
-
         }
 } else if (action.type === actions.NEW_GAME) {
-    console.log('Hello World');
     var secretNumber = Math.floor((Math.random() * 100) + 1);
     return state.concat({
         randomNumber: secretNumber,
         guesses: []
     });
 }
-
-    //var newGameReducer = function(action) {
-    //
-    //};
-    /*
-    else if (action.type === actions.RATE_REPOSITORY) {
-        // Find the index of the matching repository
-        var index = -1;
-        for (var i=0; i<state.length; i++) {
-            var repository = state[i];
-            if (repository.name === action.repository) {
-                index = i;
-                break;
-            }
-        }
-
-        if (index === -1) {
-            throw new Error('Could not find repository');
-        }
-
-        var before = state.slice(0, i);
-        var after = state.slice(i + 1);
-        var newRepository = Object.assign({}, repository, {rating: action.rating});
-        return before.concat(newRepository, after);
-    }
-    */
     return state;
 };
 
