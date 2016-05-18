880601e929e40d6d339a9d1cc41cdefdbce1f3eb
Kaeside Iwuagwu <kaeside.iwuagwu@gmail.com>
2016-05-18 10:24:35 -0400

    fix prevent default on submission of guess

--- js/formComponent.jsx
+++ js/formComponent.jsx
@@ -1,11 +1,11 @@
 var React = require('react');
-preventRefresh: function(e) {
-    e.preventDefault;
-}
 var FormComponent = React.createClass({
+    preventRefresh: function(e) {
+        e.preventDefault;
+    },
     render: function() {
         return(
-            <form onsubmit={this.preventRefresh}>
+            <form onSubmit={this.preventRefresh}>
                 <input type="text" name="userGuess" id="userGuess" className="text" maxlength="3" autocomplete="off"
                        placeholder="Enter your Guess" required/>
                 <input type="submit" id="guessButton" className="button" name="submit" value="Guess"/>
